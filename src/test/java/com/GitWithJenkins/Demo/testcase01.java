9e9499af69ae5b3aeecf7a0f4851cedfc7a9c483
Dinh263 <dinh.test263@hotmail.com>
2018-04-25 20:41:02 +0700

    first commit

---
+++ src/test/java/com/GitWithJenkins/Demo/testcase01.java
@@ -0,0 +1,11 @@
+package com.GitWithJenkins.Demo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class testcase01 {
+	@Test
+	public void testAdd() {
+		Assert.assertTrue(true);
+	}
+}
---
+++ src/test/java/com/GitWithJenkins/Demo/testcase02.java
@@ -0,0 +1,11 @@
+package com.GitWithJenkins.Demo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class testcase02 {
+	@Test
+	public void testSubtract() {
+		Assert.assertTrue(true);
+	}
+}
